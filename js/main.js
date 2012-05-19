5fb312ed2743052eb241a30a85a6737e8ff13f3b
Jr <jr@3on.fr>
2012-05-18 18:28:25 -0700

    lot's of stuff

--- js/main.js
+++ js/main.js
@@ -6,21 +6,90 @@ function anim(e, CSSClass) {
 
 	setTimeout(function() {
 		$(e).removeClass(CSSClass);
-	}, 1000);
+	}, 500);
+}
+
+function templating (tmpl, data, selector) {
+	$(selector).empty();
+	var template = _.template($(tmpl).html(), data);
+	$(selector).append(template);
+}
+
+function squaredSized(width, height) {
+  var w = 0, h = 0;
+
+  if(width <= height) {
+    w = width;
+    h = width;
+  } else {
+    w = height;
+    h = height;
+  }
+
+  return {sWidth: w, sHeight: h, sx: 0, sy: 0};
+}
+
+function resize (urlData, canvas) {
+	
+}
+
+
+function addPic (file, i) {
+	var reader = new FileReader();
+
+	reader.onload = function(e) {
+		var urlData = e.target.result;
+		var selector = $("#grid .cell")[i];
+		
+		templating("#tmpl-write", {}, selector);
+		var canvas = $(selector).find('canvas')[0];
+		var ctx = canvas.getContext('2d');
+		var image = new Image();
+
+		image.onload = function() {
+			var dim = squaredSized(image.width, image.height);
+			ctx.drawImage(image, dim.sx, dim.sy, dim.sWidth, dim.sHeight, 0, 0, 160, 160);
+		};
+
+		image.src = urlData;
+
+	};
+
+	reader.readAsDataURL(file);
+}
+
+
+function bindings () {
+	$(".cell").each(function(i) {
+		this.ondrop = function (e){
+			e.preventDefault();
+
+			anim(this, 'cellDrop');
+			
+			var file = e.dataTransfer.files[e.dataTransfer.files.length - 1];
+			addPic(file, i);
+
+			console.log(file);
+		};
+
+		this.ondragenter = function(e){
+			anim(this, 'cellDragEnter')
+		}
+	});
 }
 
 
 $(function() {
+	bindings();
+});
 
-	$(".cell").each(function() {
-		this.ondrop = function (e){
-			e.preventDefault();
 
-			anim(this, 'cellDrop')
+require(['dotcloud'], function(dotcloud){
+	dotcloud.ready(function(dotcloud){
+		window.pics = dotcloud.sync.synchronize('pics');
 
-			console.log(e.dataTransfer.files)
-		};
+		pics.observe(function(type, change){
+			console.log("change of type: " + type + " with ", change);
+		});
 	});
-
-
 });
